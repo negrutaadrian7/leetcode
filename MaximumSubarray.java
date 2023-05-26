f2a9dd65f4cb57b64af69ea40a6a9865e2346498
NEGRUTA Adrian <negrutaadrian@gmail.com>
2023-05-26 12:10:38 +0200

    Practice

---
+++ MaximumSubarray.java
@@ -0,0 +1,11 @@
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int prefix = 0;
+        int[] pref = new int[nums.length];
+        pref[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+        }
+    }
+}
